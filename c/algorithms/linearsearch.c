78addfcdbc770ce42f37cb844bc23889ee524e84
Vidhi-TEST <test9.vidhi@gmail.com>
2024-10-23 11:09:26 +0530

    CDT-15 (task) Modify linear search array

--- c/algorithms/linearsearch.c
+++ c/algorithms/linearsearch.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int linsearch(char **yarr, char *val)
 {
-    for(int i = 0; i < sizeof(yarr); i++)
+    for(int i = 0; i < sizeof(yarr) / sizeof(yarr[0]); i++)
     {
         if(strcmp(yarr[i], val) == 0)
         {
