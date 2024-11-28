dffb82bc1e17488e2e8060a3e3557a0789af6f18
Vidhi-TEST <test9.vidhi@gmail.com>
2024-11-28 16:31:25 +0530

    GMC-4 (task) refactor linear search code in c language

--- c/algorithms/linearsearch.c
+++ c/algorithms/linearsearch.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int linsearch(char **yarr, char *val, int size)
-{
-    for(int i = 0; i < size; i++)
-    {
-        if(strcmp(yarr[i], val) == 0)
-        {
+int linsearch(char **yarr, char *val, int size) {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(yarr[i], val) == 0) {
             return 0;
-        }    
+        }
     }
     return 1;
 }
 
-int main()
-{
+int main() {
     char *yarr[] = {"Shluck much", "much Shluck"};
     char *val = "Shluck Much";
     int size = sizeof(yarr) / sizeof(yarr[0]);
     linsearch(yarr, val, size);
+    return 0;
 }
