c40d507b7881ddf201cd4856ac739ac7a1e47a3c
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-05 18:00:15 +0530

    GMC-27 (task) Write detailed unit tests for each file

--- c/algorithms/BST.c
+++ c/algorithms/BST.c
@@ -1,69 +1,46 @@
-// Implementation of Binary Search Tree 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct BST {
-    int data;
-    struct BST* left;
-    struct BST* right;
-};
-
-struct BST *CreateNode() {
-    struct BST* new = (struct BST*) malloc(sizeof(struct BST));
-    new->left = NULL;
-    new->right = NULL;
-    return new; 
-};
-
-void Insert(struct BST** RootPtr, int value) {
-    struct BST* temp = *RootPtr;
-    if (temp == NULL) { /*When list is empty*/
-        struct BST* NewNode = CreateNode();
-        NewNode->data = value;
-        *RootPtr = NewNode;
-    } else if (value <= temp->data) { /*If user value is less then current node value insert in left of the node...*/
-        struct BST* NewNode = CreateNode();
-        NewNode->data = value;
-        temp->left = NewNode;
-    } else { /*If user value is greater then current node value insert at right of the node*/
-        struct BST* NewNode = CreateNode();
-        NewNode->data = value;
-        temp->right = NewNode;
-    }
+void test_Insert() {
+    struct BST* RootPtr = NULL;
+    Insert(&RootPtr, 5);
+    Insert(&RootPtr, 3);
+    Insert(&RootPtr, 7);
+    Insert(&RootPtr, 2);
+    Insert(&RootPtr, 4);
+    Insert(&RootPtr, 6);
+    Insert(&RootPtr, 8);
+    Insert(&RootPtr, 1);
+    Insert(&RootPtr, 9);
+    Insert(&RootPtr, 0);
+    // TODO: Add assertions to check if the tree is constructed correctly
 }
 
-int Search(struct BST* RootPtr, int item) { /*Implemented search using recursion*/
-    if(RootPtr == NULL) {
-        return 0; /*Returns 0 if list is empty*/
-    } else if(item == RootPtr->data) {
-        return 1; /*Returns 1 when element found*/
-    } else if(item < RootPtr->data) {
-        return Search(RootPtr->left, item); /*Otherwise search in left side of binary tree if searching value is less then the current node value*/
-    } else {
-        return Search(RootPtr->right, item); /*Otherwise search in right side of binary tree if searching value is greater then the current node value*/
-    }
+void test_Search() {
+    struct BST* RootPtr = NULL;
+    Insert(&RootPtr, 5);
+    Insert(&RootPtr, 3);
+    Insert(&RootPtr, 7);
+    Insert(&RootPtr, 2);
+    Insert(&RootPtr, 4);
+    Insert(&RootPtr, 6);
+    Insert(&RootPtr, 8);
+    Insert(&RootPtr, 1);
+    Insert(&RootPtr, 9);
+    Insert(&RootPtr, 0);
+
+    // Test cases for Search
+    int result = Search(RootPtr, 5);
+    // TODO: Add assertions to check if the result is correct
+
+    result = Search(RootPtr, 10);
+    // TODO: Add assertions to check if the result is correct
 }
 
 int main() {
-    struct BST* RootPtr = NULL;
-    int item, cont, key;
-    do {
-        printf("Enter item: ");
-        scanf("%d",&item);
-        Insert(&RootPtr, item);
-
-        printf("\n1 to keep inserting/ 0 to Exit: ");
-        scanf("%d",&cont);
-    } while(cont == 1);
-
-    printf("\nEnter element to search: ");
-    scanf("%d",&key);
-
-    if(Search(RootPtr, key) == 0) {
-        printf("\nFound\n");
-    } else {
-        printf("\nNot Found\n");
-    }
+    test_Insert();
+    test_Search();
 
     return 0;
 }
