4e8e2f2d2f27471a8b3ce81287c6b405f22587c1
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-13 17:55:33 +0530

    TP-2 (task) Write unit test cases

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 void bubble_sort(int *array, int len) {
     for (int i = 0; i < len; i++) {
@@ -12,14 +14,40 @@ void bubble_sort(int *array, int len) {
     }
 }
 
+void test_bubble_sort() {
+    int arr1[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int len1 = sizeof(arr1) / sizeof(arr1[0]);
+    bubble_sort(arr1, len1);
+    for (int i = 0; i < len1 - 1; i++) {
+        assert(arr1[i] <= arr1[i + 1]);
+    }
+
+    int arr2[] = {5, 4, 3, 2, 1};
+    int len2 = sizeof(arr2) / sizeof(arr2[0]);
+    bubble_sort(arr2, len2);
+    for (int i = 0; i < len2 - 1; i++) {
+        assert(arr2[i] <= arr2[i + 1]);
+    }
+
+    int arr3[] = {1, 2, 3, 4, 5};
+    int len3 = sizeof(arr3) / sizeof(arr3[0]);
+    bubble_sort(arr3, len3);
+    for (int i = 0; i < len3 - 1; i++) {
+        assert(arr3[i] <= arr3[i + 1]);
+    }
+}
+
 int main(void) {
+    test_bubble_sort();
+
     int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
-    bubble_sort(yarr, 10);
+    int len = sizeof(yarr) / sizeof(yarr[0]);
+
+    bubble_sort(yarr, len);
 
     printf("[");
-    for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
+    for (int i = 0; i < len; i++)
+        printf("%d, ", yarr[i]);
     printf("\b\b]\n");
     return 0;
 }
--- c/algorithms/count.c
+++ c/algorithms/count.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <assert.h>
 
 int count(int num) 
 {
@@ -21,8 +22,27 @@ int count(int num)
     }
 }
 
+void test_count()
+{
+    // Test case 1: num > 20
+    printf("Test case 1: num > 20\n");
+    printf("Expected output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]\n");
+    printf("Actual output: ");
+    count(21);
+    printf("\n\n");
+    
+    // Test case 2: num <= 20
+    printf("Test case 2: num <= 20\n");
+    printf("Expected output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]\n");
+    printf("Actual output: ");
+    count(20);
+    printf("\n\n");
+}
+
 int main()
 {
+    test_count();
+    
     int num;
     printf("Count:~$ ");
     scanf("%d", &num);
