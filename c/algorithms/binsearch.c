678b794a8d026663448f91a667cede2179e83c64
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-19 15:56:50 +0530

    GHT-5 (task) Write detailed unit test cases & refactor code

--- c/algorithms/binsearch.c
+++ c/algorithms/binsearch.c
@@ -1,48 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int binsearch(int yarr[], int size, int element)
-{
-    int left = 0;
-    int right = size - 1;
-    int mid;
-    
-    while (left <= right)
-    {
-        mid = left + (right - left) / 2;
-        
-        if (yarr[mid] == element)
-        {
+int binarySearch(int arr[], int left, int right, int x) {
+    if (right >= left) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == x)
             return mid;
-        }
-        else if (yarr[mid] < element)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
+
+        if (arr[mid] > x)
+            return binarySearch(arr, left, mid - 1, x);
+
+        return binarySearch(arr, mid + 1, right, x);
     }
-    
+
     return -1;
 }
 
-int main(int argc, char *argv[])
-{
-    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    int size = sizeof(yarr) / sizeof(yarr[0]);
-    int element = atoi(argv[1]);
-    
-    int result = binsearch(yarr, size, element);
-    
-    if (result != -1)
-    {
-        printf("Element found at index %d\n", result);
-    }
+void testBinarySearch() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = binarySearch(arr, 0, n - 1, x);
+    if (result == -1)
+        printf("Element not present in array\n");
     else
-    {
-        printf("Element not found\n");
-    }
-    
+        printf("Element found at index %d\n", result);
+}
+
+int main() {
+    testBinarySearch();
     return 0;
 }
