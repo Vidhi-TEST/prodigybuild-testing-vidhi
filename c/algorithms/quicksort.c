78bbbb4269b1ac731f072df6ca0e7c6b90b5dc7e
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-19 15:17:33 +0530

    GHT-2 (task) Refactor the code and write detailed unit test cases

--- c/algorithms/quicksort.c
+++ c/algorithms/quicksort.c
@@ -5,29 +5,48 @@ void swap(int *a, int *b);
 int partition(int *a, int l, int h);
 void quick_sort(int *a, int l, int h);
 
-int main()
+void test_quick_sort()
 {
-    int size;
-    printf("Size: ");
-    scanf("%d", &size);
-
-    int *arr = malloc(size * sizeof(int));
-    printf("Array elements: ");
-    for (int i = 0; i < size; i++)
+    int arr1[] = {5, 2, 8, 1, 9};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    quick_sort(arr1, 0, size1 - 1);
+    for (int i = 0; i < size1; i++)
     {
-        scanf("%d", &arr[i]);
-    }
-
-    quick_sort(arr, 0, size - 1);
-
-    printf("Sorted array:");
-    for (int i = 0; i < size; i++)
-    {
-        printf(" %d", arr[i]);
+        printf("%d ", arr1[i]);
     }
     printf("\n");
 
-    free(arr);
+    int arr2[] = {10, 5, 3, 7, 2};
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+    quick_sort(arr2, 0, size2 - 1);
+    for (int i = 0; i < size2; i++)
+    {
+        printf("%d ", arr2[i]);
+    }
+    printf("\n");
+
+    int arr3[] = {1, 2, 3, 4, 5};
+    int size3 = sizeof(arr3) / sizeof(arr3[0]);
+    quick_sort(arr3, 0, size3 - 1);
+    for (int i = 0; i < size3; i++)
+    {
+        printf("%d ", arr3[i]);
+    }
+    printf("\n");
+
+    int arr4[] = {5, 4, 3, 2, 1};
+    int size4 = sizeof(arr4) / sizeof(arr4[0]);
+    quick_sort(arr4, 0, size4 - 1);
+    for (int i = 0; i < size4; i++)
+    {
+        printf("%d ", arr4[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    test_quick_sort();
     return 0;
 }
 
