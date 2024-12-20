3da4964f4b554d66dc01b85fb51eea7b2a137154
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-20 15:23:04 +0530

    GT-9 (task) write unit test cases & refactor code

--- c/algorithms/selection_sort.c
+++ c/algorithms/selection_sort.c
@@ -1,86 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void print_arr(int *ptr, int size)
-{
-    printf("Before sorting: [");
-    for(int i = 0; i < size; i++)
-    {
-        printf("%d", ptr[i]);
-        if(i != size - 1)
-            printf(", ");
+int linsearch(char **yarr, char *val, int size) {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(yarr[i], val) == 0) {
+            return 0;
+        }
     }
-    printf("]\n");
+    return 1;
 }
 
-void swap(int *a, int *b)
-{
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-int *find_min(int *ptr, int size)
-{
-    int *min = ptr;
-    for(int i = 1; i < size; i++)
-    {
-        if(ptr[i] < *min)
-            min = &ptr[i];
-    }
-    return min;
-}
-
-void selection_sort(int *ptr, int size)
-{
-    for(int i = 0; i < size - 1; i++)
-    {
-        int *min = find_min(ptr + i, size - i);
-        if(*min < ptr[i])
-            swap(min, &ptr[i]);
-    }
-}
-
-void fill(char **av, int *ptr, int size)
-{
-    for(int i = 0; i < size; i++)
-    {
-        ptr[i] = atoi(av[i + 2]);
-    }
-}
-
-int main(int argc, char *argv[])
-{
-    if(argc < 3)
-    {
-        puts("Usage: ./your-executable-name [array size] [array]");
-        puts("Example: ./your-executable-name 3 2 1 0");
-        return EXIT_FAILURE;
-    }
-    int size = atoi(argv[1]);
-    if(!size)
-    {
-        puts("Error: size of array can't be 0");
-        return EXIT_FAILURE;
-    }
-    int *arr = (int *)malloc(size * sizeof(int));
-    if(!arr)
-        return EXIT_FAILURE;
-    fill(argv, arr, size);
-
-    print_arr(arr, size);
-
-    selection_sort(arr, size);
-
-    printf("After sorting:  [");
-    for(int i = 0; i < size; i++)
-    {
-        printf("%d", arr[i]);
-        if(i != size - 1)
-            printf(", ");
-    }
-    printf("]\n");
-
-    free(arr);
-    return EXIT_SUCCESS;
+int main() {
+    char *yarr[] = {"Shluck much", "much Shluck"};
+    char *val = "Shluck Much";
+    int size = sizeof(yarr) / sizeof(yarr[0]);
+    linsearch(yarr, val, size);
+    return 0;
 }
