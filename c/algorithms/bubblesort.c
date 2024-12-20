12c00bfb5400250cd55ca5190f5238d52dd820a8
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-20 15:41:40 +0530

    GMC-34 (task) write unit tests and modify code

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void bubble_sort(int *array, int len) {
     for (int i = 0; i < len; i++) {
@@ -12,14 +13,31 @@ void bubble_sort(int *array, int len) {
     }
 }
 
-int main(void) {
-    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
-    bubble_sort(yarr, 10);
+void test_bubble_sort() {
+    int test1[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int len1 = sizeof(test1) / sizeof(test1[0]);
+    bubble_sort(test1, len1);
+    for (int i = 0; i < len1 - 1; i++) {
+        if (test1[i] > test1[i + 1]) {
+            printf("Bubble Sort Test 1 Failed\n");
+            return;
+        }
+    }
+    printf("Bubble Sort Test 1 Passed\n");
 
-    printf("[");
-    for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
-    printf("\b\b]\n");
+    int test2[] = {5, 4, 3, 2, 1};
+    int len2 = sizeof(test2) / sizeof(test2[0]);
+    bubble_sort(test2, len2);
+    for (int i = 0; i < len2 - 1; i++) {
+        if (test2[i] > test2[i + 1]) {
+            printf("Bubble Sort Test 2 Failed\n");
+            return;
+        }
+    }
+    printf("Bubble Sort Test 2 Passed\n");
+}
+
+int main(void) {
+    test_bubble_sort();
     return 0;
 }
