c29e45e74d8bc361df3d21f81e2580b77d7f728f
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-20 13:18:45 +0530

    GT-7 (task) refactor the code and write unit test cases

--- c/algorithms/bubblesort.c
+++ c/algorithms/bubblesort.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void bubble_sort(int *array, int len) {
     for (int i = 0; i < len; i++) {
@@ -12,14 +14,42 @@ void bubble_sort(int *array, int len) {
     }
 }
 
-int main(void) {
-    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
-    
-    bubble_sort(yarr, 10);
+void test_bubble_sort() {
+    int test1[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    int len1 = sizeof(test1) / sizeof(test1[0]);
+    bubble_sort(test1, len1);
+    for (int i = 0; i < len1 - 1; i++) {
+        if (test1[i] > test1[i + 1]) {
+            printf("Test case 1 failed\n");
+            return;
+        }
+    }
+    printf("Test case 1 passed\n");
 
-    printf("[");
-    for (int i = 0; i < 10; i++)
-        printf("%d, ", yarr[i]);	
-    printf("\b\b]\n");
+    int test2[] = {5, 4, 3, 2, 1};
+    int len2 = sizeof(test2) / sizeof(test2[0]);
+    bubble_sort(test2, len2);
+    for (int i = 0; i < len2 - 1; i++) {
+        if (test2[i] > test2[i + 1]) {
+            printf("Test case 2 failed\n");
+            return;
+        }
+    }
+    printf("Test case 2 passed\n");
+
+    int test3[] = {1, 2, 3, 4, 5};
+    int len3 = sizeof(test3) / sizeof(test3[0]);
+    bubble_sort(test3, len3);
+    for (int i = 0; i < len3 - 1; i++) {
+        if (test3[i] > test3[i + 1]) {
+            printf("Test case 3 failed\n");
+            return;
+        }
+    }
+    printf("Test case 3 passed\n");
+}
+
+int main(void) {
+    test_bubble_sort();
     return 0;
 }
